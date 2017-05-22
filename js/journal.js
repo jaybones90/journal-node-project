38ad5fa43848f4c78a66dd64ca033152891c2e92
Jason Ainsworth and Sean Pierce <jason@>
2017-05-22 12:02:55 -0700

    initialize files and directories

---
+++ js/journal.js
@@ -0,0 +1,36 @@
+function Entry(title, body) {
+  this.title = title;
+  this.body = body;
+  this.checkLetters();
+  this.numWords();
+}
+
+Entry.prototype.numWords = function() {
+  var bodyArray =  this.body.split(' ');
+  this.numWords = bodyArray.length;
+};
+
+Entry.prototype.checkLetters = function() {
+  var bodyArray =  this.body.split('');
+  var vowelCount = [];
+  var consonantCount = [];
+  bodyArray.forEach(function(letter) {
+    if (/[a,e,i,o,u]/.test(letter)) {
+      vowelCount.push(letter);
+    } else {
+      consonantCount.push(letter);
+    };
+  });
+  this.vowels = vowelCount.length;
+  this.consonants = consonantCount.length;
+};
+
+Entry.prototype.getTeaser = function() {
+  var firstSentence = this.body.split('.')[0];
+  var teaserArr = firstSentence.split(' ')
+  while (teaserArr.length > 8) {
+    teaserArr.pop();
+  }
+  var teaserSentence = teaserArr.join(' ');
+  return `${teaserSentence}...`
+}
